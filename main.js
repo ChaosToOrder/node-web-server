eb184291891ef754a440675a07c7e7c241922d61
ChaosToOrder <1930754336@qq.com>
2019-08-09 01:47:31 +0800

    导入项目

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const express = require("express"),
     app = express()
 
 app.listen(80, () => {
-    console.log("服务器已启动...")
+    console.log("服务器正在运行...")
 })
 
 app.use(express.static('./dist/'))
